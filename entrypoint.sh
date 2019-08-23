55f7320d646352af6d205f5e7fe55627e0359cb0
RekGRpth <rekgrpth@gmail.com>
2019-08-23 07:54:59 +0500

    up

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,19 @@
 
 if [ "$GROUP" != "" ]; then
     if [ "$GROUP_ID" = "" ]; then GROUP_ID=$(id -g "$GROUP"); fi
-    if [ "$GROUP_ID" != "$(id -g "$GROUP")" ]; then groupmod --gid "$GROUP_ID" "$GROUP"; fi
+    if [ "$GROUP_ID" != "$(id -g "$GROUP")" ]; then
+        find / -group "$GROUP" -exec chgrp "$GROUP_ID" {} \; &
+        groupmod --gid "$GROUP_ID" "$GROUP"
+    fi
+    find "$HOME" ! -group "$GROUP" -exec chgrp "$GROUP_ID" {} \; &
 fi
 if [ "$USER" != "" ]; then
     if [ "$USER_ID" = "" ]; then USER_ID=$(id -u "$USER"); fi
-    if [ "$USER_ID" != "$(id -u "$USER")" ]; then usermod --uid "$USER_ID" "$USER"; fi
+    if [ "$USER_ID" != "$(id -u "$USER")" ]; then
+        find / -user "$USER" -exec chown "$USER_ID" {} \; &
+        usermod --uid "$USER_ID" "$USER"
+    fi
+    find "$HOME" ! -user "$USER" -exec chown "$USER_ID" {} \; &
 fi
 
 exec "$@"
