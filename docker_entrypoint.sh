033b268a0432af2ab852c2cda57377ecf2bba73c
RekGRpth <rekgrpth@gmail.com>
2020-04-09 08:11:23 +0500

    up

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -4,12 +4,14 @@ if [ "$GROUP" != "" ]; then
     if [ "$GROUP_ID" = "" ]; then GROUP_ID=$(id -g "$GROUP"); fi
     if [ "$GROUP_ID" != "$(id -g "$GROUP")" ]; then
         groupmod --gid "$GROUP_ID" "$GROUP"
+        chgrp "$GROUP_ID" "$HOME"
     fi
 fi
 if [ "$USER" != "" ]; then
     if [ "$USER_ID" = "" ]; then USER_ID=$(id -u "$USER"); fi
     if [ "$USER_ID" != "$(id -u "$USER")" ]; then
         usermod --uid "$USER_ID" "$USER"
+        chown "$USER_ID" "$HOME"
     fi
     exec "$@"
 else
