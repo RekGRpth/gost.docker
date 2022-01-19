b8e2b47e8a417df689e41abbe45d12b288936633
RekGRpth <rekgrpth@gmail.com>
2022-01-19 19:58:11 +0500

    rw

---
+++ bin/docker_gost.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -eux
+
+sed -i '6i openssl_conf=openssl_def' "$1"
+cat >>"$1" <<EOF
+
+# OpenSSL default section
+[openssl_def]
+engines = engine_section
+
+# Engine scetion
+[engine_section]
+gost = gost_section
+
+# Engine gost section
+[gost_section]
+engine_id = gost
+default_algorithms = ALL
+CRYPT_PARAMS = id-Gost28147-89-CryptoPro-A-ParamSet
+EOF
