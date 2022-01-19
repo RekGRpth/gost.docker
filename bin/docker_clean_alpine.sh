80e1f0db3f9da26b82b189b1888499311e52f990
RekGRpth <rekgrpth@gmail.com>
2022-01-19 18:37:54 +0500

    fix

--- bin/docker_clean_alpine.sh
+++ bin/docker_clean_alpine.sh
@@ -9,7 +9,7 @@ apk add --no-cache --virtual .gost-rundeps \
     shadow \
     su-exec \
     tzdata \
-    $(scanelf --needed --nobanner --format '%n#p' --recursive /usr/local | tr ',' '\n' | sort -u | while read -r lib; do test ! -e "/usr/local/lib/$lib" && echo "so:$lib"; done) \
+    $(scanelf --needed --nobanner --format '%n#p' --recursive /usr/local | tr ',' '\n' | sort -u | while read -r lib; do test -z "$(find /usr/local/lib -name "$lib")" && echo "so:$lib"; done) \
 ;
 find /usr/local/bin -type f -exec strip '{}' \;
 find /usr/local/lib -type f -name "*.so" -exec strip '{}' \;
